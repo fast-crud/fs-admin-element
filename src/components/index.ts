7aa406a4952561cecf28ed4cdc2756c8b70e093d
xiaojunnuo <xiaojunnuo@qq.com>
2021-12-22 01:07:06 +0800

    perf: fs-page内置到fast-crud

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,10 +1,8 @@
 import { defineAsyncComponent } from "vue";
-import FsPage from "./fs-page.vue";
 const AsyncHighLight = defineAsyncComponent(() => import("./highlight/index.vue"));
 
 export default {
   install(app) {
     app.component("FsHighlight", AsyncHighLight);
-    app.component("FsPage", FsPage);
   }
 };
