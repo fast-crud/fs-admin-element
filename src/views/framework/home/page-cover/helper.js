24788be66b03823f5cc8f31356168309db3fd647
xiaojunnuo <xiaojunnuo@qq.com>
2021-12-31 22:54:28 +0800

    refactor: home

--- src/views/framework/home/page-cover/helper.js
+++ src/views/framework/home/page-cover/helper.js
@@ -1,16 +1,11 @@
 export default {
   crud: `  columns: [
     date:{
-      title: '日期', //字段名称
-      type: 'date', //字段类型，添加、修改、查询将自动生成相应表单组件
-    },
-    status: {
-      title: '状态',
-      type: 'dict-select', //选择框，默认单选
-      dict: dict({ url: '/dicts/OpenStatusEnum' })//远程数据字典
+      title: '姓名', //字段名称
+      type: 'text', //字段类型，添加、修改、查询将自动生成相应表单组件
     },
     province: {
-      title: '地区',
+      title: '城市',
       type: 'dict-select', //选择框
       form: { //表单组件自定义配置，此处配置选择框为多选
         component: { //支持任何v-model组件
@@ -25,7 +20,12 @@ export default {
           { value: 'sh', label: '上海' }
         ]
       })
-    }
+    },
+     status: {
+      title: '状态',
+      type: 'dict-select', //选择框，默认单选
+      dict: dict({ url: '/dicts/OpenStatusEnum' })//远程数据字典
+    },
   ]
   `
 };
