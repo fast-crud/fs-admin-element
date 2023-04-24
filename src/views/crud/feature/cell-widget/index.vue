0fddd86189713c350299f6a20cc8f7e0354572c9
mayihua <1@1.com>
2023-04-24 15:48:12 +0800

    格式化代码

--- src/views/crud/feature/cell-widget/index.vue
+++ src/views/crud/feature/cell-widget/index.vue
@@ -1,16 +1,16 @@
 <template>
   <fs-page>
-    <fs-crud ref="crudRef" v-bind="crudBinding"/>
+    <fs-crud ref="crudRef" v-bind="crudBinding" />
   </fs-page>
 </template>
 
 <script lang="ts">
-import {defineComponent, ref, onMounted} from 'vue';
-import createCrudOptions from './crud';
-import {useExpose, useCrud} from '@fast-crud/fast-crud';
+import { defineComponent, ref, onMounted } from "vue";
+import createCrudOptions from "./crud";
+import { useExpose, useCrud } from "@fast-crud/fast-crud";
 
 export default defineComponent({
-  name: 'CellWidget',
+  name: "CellWidget",
   props: {
     modelValue: {}
   },
@@ -20,12 +20,12 @@ export default defineComponent({
     // crud 配置的ref
     const crudBinding = ref();
     // 暴露的方法
-    const {expose} = useExpose({crudRef, crudBinding});
+    const { expose } = useExpose({ crudRef, crudBinding });
     // 你的crud配置
-    const {crudOptions} = createCrudOptions({expose});
+    const { crudOptions } = createCrudOptions({ expose });
     // 初始化crud配置
     // eslint-disable-next-line @typescript-eslint/no-unused-vars,no-unused-vars
-    const {resetCrudOptions} = useCrud({expose, crudOptions});
+    const { resetCrudOptions } = useCrud({ expose, crudOptions });
     // 你可以调用此方法，重新初始化crud配置
     // resetCrudOptions(options)
 
@@ -34,52 +34,13 @@ export default defineComponent({
     //   expose.doRefresh();
     // });
 
-
-    // 通过crudBinding.value.data 可以获取表格实时数据
-    function showData() {
-      console.log('data:', crudBinding.value.data)
-    }
-
     //初始化本地数据示例
-    crudBinding.value.data = [{name: 'test', mobile:""}]
-
-
-    //启用行编辑
-    function enabledRowEdit() {
-      expose.editable.enable({
-        mode: "row",
-      });
-      crudBinding.value.actionbar.buttons.add.show = false
-      crudBinding.value.actionbar.buttons.addRow.show = true
-
-    }
-
-    //启用自由编辑
-    function enabledFreeEdit() {
-      expose.editable.enable({
-        mode: "free",
-      });
-      crudBinding.value.actionbar.buttons.add.show = false
-      crudBinding.value.actionbar.buttons.addRow.show = true
-    }
-
-    //启用关闭编辑模式
-    function disabledEdit() {
-      expose.editable.enable({
-        enabled: false
-      });
-      crudBinding.value.actionbar.buttons.add.show = true
-      crudBinding.value.actionbar.buttons.addRow.show = false
-    }
+    crudBinding.value.data = [{ name: "test", mobile: "" }];
 
     return {
       crudBinding,
-      crudRef,
-      showData,
-      enabledRowEdit,
-      enabledFreeEdit,
-      disabledEdit
+      crudRef
     };
-  },
+  }
 });
 </script>
