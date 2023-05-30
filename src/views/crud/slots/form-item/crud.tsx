dbd7fd8ce008028231a051bd33cedd4748ab3818
hedongyang <2732067273@qq.com>
2023-05-30 22:38:35 +0800

    perf：表单插槽新增校验示例

--- src/views/crud/slots/form-item/crud.tsx
+++ src/views/crud/slots/form-item/crud.tsx
@@ -1,4 +1,6 @@
 import * as api from "./api";
+import type { FormRules } from 'element-plus';
+import { reactive } from "vue";
 import { CreateCrudOptionsProps, CreateCrudOptionsRet } from "@fast-crud/fast-crud";
 
 export default function ({ expose }: CreateCrudOptionsProps): CreateCrudOptionsRet {
@@ -16,6 +18,15 @@ export default function ({ expose }: CreateCrudOptionsProps): CreateCrudOptionsR
   const addRequest = async ({ form }) => {
     return await api.AddObj(form);
   };
+  const checkNum = (rule, value, callback) => {
+    if (value === '') {
+      return callback(new Error('全局form校验'));
+    }
+    if (!/^[0-9]*$/.test(value)) {
+      return callback(new Error('全局form校验'));
+    }
+    return callback();
+  };
   return {
     crudOptions: {
       request: {
@@ -23,6 +34,27 @@ export default function ({ expose }: CreateCrudOptionsProps): CreateCrudOptionsR
         addRequest,
         editRequest,
         delRequest
+      },
+      form: {
+        // 单列布局
+        col: { span: 24 },
+        // 仅在独立使用表单组件时有效，fs-crud中会被行数据代替，你可以在字段中配置columns.key.form.value=默认值
+        initialForm: {
+          numBe: "1"
+        },
+        // 插槽的表单校验在这配置
+        // 数组的表单校验看https://element-plus.org/zh-CN/component/form.html#%E6%B7%BB%E5%8A%A0-%E5%88%A0%E9%99%A4%E8%A1%A8%E5%8D%95%E9%A1%B9
+        rules: reactive<FormRules>({
+          operator: [
+            { required: true, message: "全局form校验" }
+          ],
+          numBe: [
+            { validator: checkNum, trigger: 'blur' }
+          ]
+          // numAf: [
+          //   { validator: checkNum, trigger: 'blur' }
+          // ]
+        })
       },
       columns: {
         id: {
@@ -36,15 +68,62 @@ export default function ({ expose }: CreateCrudOptionsProps): CreateCrudOptionsR
             show: false
           }
         },
+        // 如果插槽字段想添加必填星号，又不想设为必填（不然必需给multiField赋值），参考/crud/form/render
+        multiField: {
+          title: "多字段录入",
+          type: "text",
+          form: {
+            // 如无必要，可不配置message，校验提示交给插槽
+            rules: [{ required: true, message: "" }]
+          }
+        },
         topics: {
           title: "多行输入",
           type: "text",
           search: { show: true },
           form: {
-            rules: [{ required: true, message: "请输入" }]
+            rules: [{ required: true, message: "请添加主题_columns.xxx.form.rules校验" }]
           },
           column: {
             component: { name: "fs-values-format" }
+          }
+        },
+        // 插槽表单的字段在这里声明，并隐藏。不然编辑弹窗显示不了这些的值
+        // （可能是mock数据的原因，如果你详情接口有二次赋值，可以看看清空下面配置是什么表现）
+        // 插槽表单字段的默认值在这配置优先级低于于父级的form.initialForm
+        // 表单校验这里配置无效
+        numBe: {
+          show: false,
+          column: {
+            show: false
+          },
+          form: {
+            show: false,
+            // 优先级低于于父级的form.initialForm
+            value: '111'
+          }
+        },
+        operator: {
+          show: false,
+          column: {
+            show: false
+          },
+          form: {
+            show: false,
+            // 无效，使用全局form校验
+            rules: [{ required: true, message: "columns.xxx.form.rules校验" }]
+          }
+        },
+        numAf: {
+          show: false,
+          column: {
+            show: false
+          },
+          form: {
+            show: false,
+            // 无效，该字段不会校验
+            rules: [{ required: true, message: "columns.xxx.form.rules校验" }],
+            value: '222'
           }
         }
       }
--- src/views/crud/slots/form-item/index.vue
+++ src/views/crud/slots/form-item/index.vue
@@ -2,26 +2,59 @@
   <fs-page>
     <fs-crud ref="crudRef" v-bind="crudBinding">
       <template #actionbar-right>
-        <el-alert class="ml-1" type="info" title=" ← form表单字段插槽，可以做一些很复杂的输入" />
+        <el-alert class="ml-1" type="info" title=" ← form表单字段插槽，可以做一些很复杂的输入，可配置校验" />
+      </template>
+      <template #form_multiField="scope">
+        <el-form-item prop="numBe" class="form-item-num"
+          v-if="scope.mode !== 'view'">
+          <el-input
+            v-model="scope.form.numBe"
+            placeholder="请输入数字"
+            @change="onMultiFieldChange(scope.form)"
+          />
+        </el-form-item>
+        <el-form-item prop="operator" class="form-item-num"
+          v-if="scope.mode !== 'view'">
+          <el-select v-model="scope.form.operator" @change="onMultiFieldChange(scope.form)">
+            <el-option
+              v-for="item in operatorList"
+              :key="item.value"
+              :label="item.label"
+              :value="item.value"
+            >
+            </el-option>
+          </el-select>
+        </el-form-item>
+        <el-form-item prop="numAf" class="form-item-num"
+          v-if="scope.mode !== 'view'">
+          <el-input
+            v-model="scope.form.numAf"
+            placeholder="请输入数字"
+            @change="onMultiFieldChange(scope.form)"
+          />
+        </el-form-item> = {{scope.form.multiField || '？' }}
       </template>
 
       <template #form_topics="scope">
-        <el-input
+        <el-form-item style="margin-bottom: 20px;"
           v-for="(item, index) in scope.form.topics"
           :key="index"
-          v-model="scope.form.topics[index]"
-          :disabled="scope.mode === 'view'"
-          class="mb-5"
-        >
-          <template #append>
-            <fs-button
+          :prop="'topics.' + index"
+          :rules="{ required: true, message: '请输入', trigger: 'blur' }">
+            <el-input
+              v-model="scope.form.topics[index]"
               :disabled="scope.mode === 'view'"
-              icon="ion:trash-outline"
-              @click="removeTopic(index, scope.form, scope.key)"
-            >
-            </fs-button>
-          </template>
-        </el-input>
+              placeholder="请输入">
+            <template #append>
+              <fs-button
+                :disabled="scope.mode === 'view'"
+                icon="ion:trash-outline"
+                @click="removeTopic(index, scope.form, scope.key)"
+              >
+              </fs-button>
+            </template>
+          </el-input>
+        </el-form-item>
         <el-button :disabled="scope.mode === 'view'" @click="addTopic(scope.form, scope.key)">添加主题</el-button>
       </template>
     </fs-crud>
@@ -53,7 +86,19 @@ export default defineComponent({
     onMounted(() => {
       expose.doRefresh();
     });
-
+    const operatorList = [
+      { value: "+", label: "加" },
+      { value: "-", label: "减" },
+      { value: "*", label: "乘" },
+      { value: "/", label: "除" }
+    ]
+    function onMultiFieldChange(form) {
+      if (form.numBe && form.numAf && form.operator) {
+        form.multiField = `${form.numBe} ${form.operator} ${form.numAf}`;
+        return
+      }
+      form.multiField = "？"
+    }
     function addTopic(form, key) {
       if (form[key] == null) {
         form[key] = [];
@@ -66,9 +111,20 @@ export default defineComponent({
     return {
       crudBinding,
       crudRef,
+      operatorList,
       addTopic,
-      removeTopic
+      removeTopic,
+      onMultiFieldChange
     };
   }
 });
 </script>
+<style lang="scss">
+.fs-form-item-component {
+  .el-form-item.form-item-num {
+    width: 120px;
+    display: inline-block;
+    margin-right: 10px;
+  }
+}
+</style>
