69796ca31eabb5681fd2f3bbf13b569887822ecc
xiaojunnuo <xiaojunnuo@qq.com>
2023-05-31 16:05:21 +0800

    fix: 修复search-middle插槽错位问题

--- src/views/crud/slots/form-item/index.vue
+++ src/views/crud/slots/form-item/index.vue
@@ -5,46 +5,30 @@
         <el-alert class="ml-1" type="info" title=" ← form表单字段插槽，可以做一些很复杂的输入，可配置校验" />
       </template>
       <template #form_multiField="scope">
-        <el-form-item prop="numBe" class="form-item-num"
-          v-if="scope.mode !== 'view'">
-          <el-input
-            v-model="scope.form.numBe"
-            placeholder="请输入数字"
-            @change="onMultiFieldChange(scope.form)"
-          />
+        <el-form-item v-if="scope.mode !== 'view'" prop="numBe" class="form-item-num">
+          <el-input v-model="scope.form.numBe" placeholder="请输入数字" @change="onMultiFieldChange(scope.form)" />
         </el-form-item>
-        <el-form-item prop="operator" class="form-item-num"
-          v-if="scope.mode !== 'view'">
+        <el-form-item v-if="scope.mode !== 'view'" prop="operator" class="form-item-num">
           <el-select v-model="scope.form.operator" @change="onMultiFieldChange(scope.form)">
-            <el-option
-              v-for="item in operatorList"
-              :key="item.value"
-              :label="item.label"
-              :value="item.value"
-            >
+            <el-option v-for="item in operatorList" :key="item.value" :label="item.label" :value="item.value">
             </el-option>
           </el-select>
         </el-form-item>
-        <el-form-item prop="numAf" class="form-item-num"
-          v-if="scope.mode !== 'view'">
-          <el-input
-            v-model="scope.form.numAf"
-            placeholder="请输入数字"
-            @change="onMultiFieldChange(scope.form)"
-          />
-        </el-form-item> = {{scope.form.multiField || '？' }}
+        <el-form-item v-if="scope.mode !== 'view'" prop="numAf" class="form-item-num">
+          <el-input v-model="scope.form.numAf" placeholder="请输入数字" @change="onMultiFieldChange(scope.form)" />
+        </el-form-item>
+        = {{ scope.form.multiField || "？" }}
       </template>
 
       <template #form_topics="scope">
-        <el-form-item style="margin-bottom: 20px;"
+        <el-form-item
           v-for="(item, index) in scope.form.topics"
           :key="index"
+          style="margin-bottom: 20px"
           :prop="'topics.' + index"
-          :rules="{ required: true, message: '请输入', trigger: 'blur' }">
-            <el-input
-              v-model="scope.form.topics[index]"
-              :disabled="scope.mode === 'view'"
-              placeholder="请输入">
+          :rules="{ required: true, message: '请输入', trigger: 'blur' }"
+        >
+          <el-input v-model="scope.form.topics[index]" :disabled="scope.mode === 'view'" placeholder="请输入">
             <template #append>
               <fs-button
                 :disabled="scope.mode === 'view'"
@@ -73,31 +57,30 @@ export default defineComponent({
     // crud 配置的ref
     const crudBinding = ref();
     // 暴露的方法
-    const { expose } = useExpose({ crudRef, crudBinding });
+    const { crudExpose } = useExpose({ crudRef, crudBinding });
     // 你的crud配置
-    const { crudOptions } = createCrudOptions({ expose });
+    const { crudOptions } = createCrudOptions({ crudExpose });
     // 初始化crud配置
-    // eslint-disable-next-line @typescript-eslint/no-unused-vars,no-unused-vars
-    const { resetCrudOptions } = useCrud({ expose, crudOptions });
+    const { resetCrudOptions } = useCrud({ crudExpose, crudOptions, context: {} });
     // 你可以调用此方法，重新初始化crud配置
     // resetCrudOptions(options)
 
     // 页面打开后获取列表数据
     onMounted(() => {
-      expose.doRefresh();
+      crudExpose.doRefresh();
     });
     const operatorList = [
       { value: "+", label: "加" },
       { value: "-", label: "减" },
       { value: "*", label: "乘" },
       { value: "/", label: "除" }
-    ]
+    ];
     function onMultiFieldChange(form) {
       if (form.numBe && form.numAf && form.operator) {
         form.multiField = `${form.numBe} ${form.operator} ${form.numAf}`;
-        return
+        return;
       }
-      form.multiField = "？"
+      form.multiField = "？";
     }
     function addTopic(form, key) {
       if (form[key] == null) {
@@ -119,7 +102,7 @@ export default defineComponent({
   }
 });
 </script>
-<style lang="scss">
+<style lang="less">
 .fs-form-item-component {
   .el-form-item.form-item-num {
     width: 120px;
