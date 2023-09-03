e73857a5dd075fb03114986f6f8393f91a8541f1
xiaojunnuo <xiaojunnuo@qq.com>
2023-09-03 16:43:00 +0800

    perf: element 增加树形懒加载示例
    https://github.com/fast-crud/fast-crud/issues/228

--- .eslintrc.js
+++ .eslintrc.js
@@ -28,7 +28,7 @@ module.exports = {
     "no-unused-vars": "off",
     "@typescript-eslint/ban-ts-ignore": "off",
     "@typescript-eslint/explicit-function-return-type": "off",
-    // "@typescript-eslint/no-explicit-any": "off",
+    "@typescript-eslint/no-explicit-any": "off",
     "@typescript-eslint/no-var-requires": "off",
     "@typescript-eslint/no-empty-function": "off",
     "@typescript-eslint/no-use-before-define": "off",
--- src/views/crud/component/tree/crud.tsx
+++ src/views/crud/component/tree/crud.tsx
@@ -1,5 +1,13 @@
 import * as api from "./api";
-import { CreateCrudOptionsProps, CreateCrudOptionsRet, dict } from "@fast-crud/fast-crud";
+import {
+  asyncCompute,
+  CreateCrudOptionsProps,
+  CreateCrudOptionsRet,
+  dict,
+  GetContextFn,
+  ScopeContext
+} from "@fast-crud/fast-crud";
+import { getRandomInt } from "element-plus/es/utils";
 
 export default function ({ expose }: CreateCrudOptionsProps): CreateCrudOptionsRet {
   const pageRequest = async (query) => {
@@ -16,6 +24,8 @@ export default function ({ expose }: CreateCrudOptionsProps): CreateCrudOptionsR
   const addRequest = async ({ form }) => {
     return await api.AddObj(form);
   };
+
+  let idGen = 0;
   return {
     crudOptions: {
       request: {
@@ -45,6 +55,77 @@ export default function ({ expose }: CreateCrudOptionsProps): CreateCrudOptionsR
             url: "/mock/dicts/cascaderData?single"
           })
         },
+        lazy: {
+          title: "懒加载",
+          search: { show: false },
+          type: "dict-tree",
+          dict: dict({
+            prototype: true,
+            value: "id",
+            label: "text",
+            async getNodesByValues(values: any) {
+              //你要实现这个方法，根据values从后台获取节点数据
+              //以下为本地转换模拟
+              return [
+                {
+                  id: values,
+                  text: "lazy load node " + values
+                }
+              ];
+            }
+          }),
+          form: {
+            component: {
+              props: {
+                props: {
+                  // 为什么这里要写两层props
+                  // 因为props属性名与fs的动态渲染的props命名冲突，所以要多写一层
+                  label: "text",
+                  value: "id",
+                  isLeaf: "leaf"
+                }
+              },
+              lazy: true,
+              cacheData: asyncCompute({
+                watch(context: ScopeContext) {
+                  return context.form.lazy;
+                },
+                async asyncFn(value: any, getContextFn: GetContextFn) {
+                  if (value == null) {
+                    return [];
+                  }
+                  //异步从后台获取节点
+                  return [
+                    {
+                      id: value,
+                      text: "lazy load node " + value
+                    }
+                  ];
+                },
+                defaultValue: []
+              }),
+              load(node: any, resolve: (data: any[]) => void) {
+                if (node.isLeaf) return resolve([]);
+
+                setTimeout(() => {
+                  //根据node.id 作为父节点，从后台加载子节点数据
+                  resolve([
+                    {
+                      id: ++idGen,
+                      text: `lazy load node ${idGen}`,
+                      leaf: false
+                    },
+                    {
+                      id: ++idGen,
+                      text: `lazy load node ${idGen}`,
+                      leaf: true
+                    }
+                  ]);
+                }, 400);
+              }
+            }
+          }
+        },
         multiple: {
           title: "多选",
           search: { show: false },
@@ -72,20 +153,12 @@ export default function ({ expose }: CreateCrudOptionsProps): CreateCrudOptionsR
           }),
           form: {
             component: {
-              select: {
-                valueKey: "code"
-              },
-              tree: {
-                valueKey: "code"
-              },
               props: {
                 props: {
                   // 为什么这里要写两层props
                   // 因为props属性名与fs的动态渲染的props命名冲突，所以要多写一层
                   label: "name",
-                  key: "code",
-                  value: "code",
-                  valueKey: "code"
+                  value: "code"
                 }
               }
             }
