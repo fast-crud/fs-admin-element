123245db371e65caa18812d7b897a33421f0511c
xiaojunnuo <xiaojunnuo@qq.com>
2024-01-25 23:25:31 +0800

    perf: 行编辑也支持排他式激活
    Closes https://github.com/fast-crud/fast-crud/issues/332

--- src/views/crud/editable/row/crud.tsx
+++ src/views/crud/editable/row/crud.tsx
@@ -1,5 +1,15 @@
 import * as api from "./api";
-import { AddReq, compute, CreateCrudOptionsProps, CreateCrudOptionsRet, DelReq, dict, EditReq, UserPageQuery, UserPageRes } from "@fast-crud/fast-crud";
+import {
+  AddReq,
+  compute,
+  CreateCrudOptionsProps,
+  CreateCrudOptionsRet,
+  DelReq,
+  dict,
+  EditReq,
+  UserPageQuery,
+  UserPageRes
+} from "@fast-crud/fast-crud";
 export default function ({ crudExpose }: CreateCrudOptionsProps): CreateCrudOptionsRet {
   const pageRequest = async (query: UserPageQuery): Promise<UserPageRes> => {
     return await api.GetList(query);
@@ -15,8 +25,7 @@ export default function ({ crudExpose }: CreateCrudOptionsProps): CreateCrudOpti
   };
 
   const addRequest = async ({ form }: AddReq) => {
-    const id = await api.AddObj(form);
-    return { id };
+    return await api.AddObj(form);
   };
 
   return {
